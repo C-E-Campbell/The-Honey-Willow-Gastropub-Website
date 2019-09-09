c5cffdc7f69a1001488d7a57557fb8cc27ecb407
charles <charles.e.campbell1989@gmail.com>
2019-09-09 10:34:15 -0400

    added 2nd modal

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const giftCardBtn = document.querySelector(".giftcards");
+const submitBtn = document.querySelector("#sendBtn");
 const modal = document.getElementById("myModal");
+const modal2 = document.getElementById("myModal2");
 const span = document.getElementsByClassName("close")[0];
+const span2 = document.getElementsByClassName("close2")[0];
+
 giftCardBtn.onclick = function() {
 	modal.style.display = "block";
 };
@@ -12,5 +16,19 @@ span.onclick = function() {
 window.onclick = function(event) {
 	if (event.target == modal) {
 		modal.style.display = "none";
+	}
+};
+
+submitBtn.onclick = function() {
+	modal2.style.display = "block";
+};
+
+span2.onclick = function() {
+	modal2.style.display = "none";
+};
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal2.style.display = "none";
 	}
 };
